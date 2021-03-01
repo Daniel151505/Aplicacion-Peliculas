f51de4e973824441d8e68fcc3036f9f0c8db8ea1
Daniel151505 <daniel150520011@gmail.com>
2021-03-01 17:36:59 -0500

    Agregando Vista Navbar

---
+++ aplicacion-peliculas/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="navbar-wrap" class="card-shadow">
+        <div id="navbar">
+            <h2>Aplicación Peliculas</h2>
+            <div>
+                <input type="text" placeholder="Encuentra tu pelicula"/>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
