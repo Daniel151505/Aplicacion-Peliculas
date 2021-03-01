9297ae825752c785fe53c7b75d9200563eb27f31
Daniel151505 <daniel150520011@gmail.com>
2021-03-01 17:39:34 -0500

    Importando Navbar

--- aplicacion-peliculas/src/views/Home.vue
+++ aplicacion-peliculas/src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Navbar from '../components/Navbar'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
