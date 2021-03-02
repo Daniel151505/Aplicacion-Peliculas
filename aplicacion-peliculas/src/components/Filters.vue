689d843223bc2ac4ed92bd148a5bb89ab8e00e0f
Daniel151505 <daniel150520011@gmail.com>
2021-03-01 19:12:13 -0500

    Agregando Estilos Filtros

--- aplicacion-peliculas/src/components/Filters.vue
+++ aplicacion-peliculas/src/components/Filters.vue
@@ -14,3 +14,14 @@ export default {
 }
 </script>
 
+<style lang='scss' scoped>
+	#filters {
+		text-align: left;
+		color: rgb(143, 143, 143);
+		cursor: pointer;
+
+		p {
+			margin-left: 10px;
+		}
+	}
+</style>
