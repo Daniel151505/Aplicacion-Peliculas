5e7ef722471ddb5e011218db6c5037787b9d2f86
Daniel151505 <daniel150520011@gmail.com>
2021-03-01 19:13:10 -0500

    Agregando Estilos Lista de Peliculas

--- aplicacion-peliculas/src/components/MovieList.vue
+++ aplicacion-peliculas/src/components/MovieList.vue
@@ -10,3 +10,12 @@ export default {
 }
 </script>
 
+<style lang='scss' scoped>
+	#movie-container {
+		padding-top: 1rem;
+		display: flex;
+		flex-wrap: wrap;
+		flex-grow: 2;
+		justify-content: center;
+	}
+</style>
