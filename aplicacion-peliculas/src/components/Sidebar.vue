6eb596dffe17fa7a34f8541c4a6def67d421c1b9
Daniel151505 <daniel150520011@gmail.com>
2021-03-01 19:14:04 -0500

    Agregando Vista Slidebar y llamando al componente Filters

---
+++ aplicacion-peliculas/src/components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="sidebar">
+      <filters/>
+	  <button>Añade Pelicula </button>
+  </div>
+</template>
+
+<script>
+import Filters from './Filters.vue'
+export default {
+	components: {
+		Filters
+	}
+}
+</script>
