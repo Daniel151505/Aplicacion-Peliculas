bfb63e0c65ca9aac2077019071c70302776e73e5
Daniel151505 <daniel150520011@gmail.com>
2021-03-01 19:11:50 -0500

    Agregando Vista Filtros

---
+++ aplicacion-peliculas/src/components/Filters.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="filters">
+      <h3>Filtros</h3>
+      <p> Calificación más alta </p>
+      <p> Calificación más baja </p>
+      <p> Año mas nuevo </p>
+      <p> Año mas viejo </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
